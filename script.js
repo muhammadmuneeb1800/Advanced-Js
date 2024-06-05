a3355890e93d79f634c2001a6797164c412fccca
Muhammad Muneeb <nadeemmk1900@gmail.com>
2024-06-05 11:54:45 +0500

    Advanced-Js

---
+++ script.js
@@ -0,0 +1,70 @@
+let users = [
+  {
+    name: "John",
+    email: "John@example.com",
+    city: "Faisalabad",
+    Age: "20",
+    id: 1,
+  },
+  {
+    name: "Muneeb",
+    email: "Muneeb@example.com",
+    city: "Lahore",
+    Age: "25",
+    id: 2,
+  },
+  {
+    name: "Hamza",
+    email: "Hamza@example.com",
+    city: "Karachi",
+    Age: "30",
+    id: 3,
+  },
+  {
+    name: "Faisal",
+    email: "Faisal@example.com",
+    city: "Quetta",
+    Age: "18",
+    id: 4,
+  },
+  {
+    name: "Rameez",
+    email: "Rameez@example.com",
+    city: "Punjab",
+    Age: "22",
+    id: 5,
+  },
+];
+
+// console.log("Users", users);
+
+// let mapUsers = users.map((user, i) => {
+//   console.log("mapUsers", user);
+// });
+
+// let FilterUsers = users.filter((user, i) => {
+//   return user.Age > 20;
+// });
+// console.log("FilterUsers", FilterUsers);
+
+// let findUsers = users.find((user, i) => {
+//   return (user.Age = 20);
+// });
+// console.log("FindUsers", findUsers);
+
+// let user = {
+//   name: "John",
+//   email: "John@example.com",
+//   city: "Faisalabad",
+//   age: 20,
+//   id: 1,
+// };
+
+// user.name;
+// user.email;
+// user.city;
+// user.age;
+
+// let { name, email, age } = user;
+// console.log({ name, email, age });
+
